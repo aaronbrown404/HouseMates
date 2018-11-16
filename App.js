21a404560058b5a1d573273e7813e7a1a0d1d337
Aaron Brown <40678694+robot404@users.noreply.github.com>
2018-11-15 16:27:40 -0800

    Updated UI for App.js

--- App.js
+++ App.js
@@ -1,20 +1,42 @@
 import React from 'react';
-import {createStackNavigator } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation';
+import { Image, View } from 'react-native';
+import { Icon, Button } from 'native-base';
 
 // These import paths may need to change depending on the directory hierarchy.
 import WelcomeScreen from './src/screens/WelcomeScreen';
 import SignUpScreen from './src/screens/SignUpScreen';
 import HouseSetupScreen from './src/screens/HouseSetupScreen';
-import WIPScreen from './src/screens/WIPScreen';
+import CreateTaskScreen from './src/screens/CreateTaskScreen';
 import TabNavigation from './src/TabNavigation';
+import LogInScreen from './src/screens/LogInScreen';
+import WIPScreen from './src/screens/WIPScreen';
 import Firebase from  './src/components/Firebase';
 
+/**
+ * Class required for top navigation bar's House Mates Logo image.
+ */
+class LogoTitle extends React.Component {
+    render() {
+        return (
+            <View style={{paddingLeft: 8}}>
+               <Image source={require('./assets/HouseMatesPNGLogo_long_noBackground.png')}
+                      style={{width: 180, height: 90}}
+                      resizeMode='contain'
+               />
+            </View>
+        );
+    }
+}
 
 // Governs screen names as well as sets up the navigator. Required for screen traversal.
 const RootStack = createStackNavigator(
     {
         Welcome: {
             screen: WelcomeScreen
+        },
+        LogIn: {
+            screen: LogInScreen
         },
         SignUp: {
             screen: SignUpScreen
@@ -25,21 +47,32 @@ const RootStack = createStackNavigator(
         TabNavigation: {
             screen: TabNavigation
         },
+        CreateTask: {
+            screen: CreateTaskScreen
+        },
         WIP: {
             screen: WIPScreen
         }
     },
     {
-
         initialRouteName: 'Welcome', // Determines starting screen.
 
-        headerMode: 'screen',
+        //headerMode: 'screen',
         navigationOptions: {
-            headerTitleStyle: {
-                fontWeight: "bold",
-                color: "#fff",
+            headerTitle: <LogoTitle />,
+            headerStyle: {
+                backgroundColor: '#283350',
             },
-            headerTintColor: "blue"
+
+            // Lacks functionality at the moment. This is the options button (three dots) at the top right
+            // of the nav bar on the ToDoListScreen.js and HouseholdScreen.js.
+            headerRight:
+                <Button transparent
+                        style={{justifyContent: 'center', alignSelf: 'center'}}>
+                    <Icon style={{color: 'white'}}
+                          name="md-more"/>
+                </Button>,
+            headerTintColor: "white"
         }
     }
 );
