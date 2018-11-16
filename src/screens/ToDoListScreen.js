7758f7ce85a05886b5d3479edd4f6e792bf5a065
Aaron Brown <40678694+robot404@users.noreply.github.com>
2018-11-15 16:43:08 -0800

    Update ToDoListScreen.js

--- src/screens/ToDoListScreen.js
+++ src/screens/ToDoListScreen.js
@@ -1,12 +1,45 @@
 import React from "react";
-import {Text, View} from "react-native";
+import { StyleSheet } from "react-native";
+import { Container, Content, Icon} from 'native-base';
 
+// WARNING! Image path may need to be updated depending on directory hierarchy.
+import CardComponent from '../ToDoListCardComponent';
+
+/**
+ * class ToDoListScreen
+ * Task completion screen. The main functionality of this screen allows the user to view tasks assigned to them and
+ * mark them off when completed.
+ */
 export default class ToDoListScreen extends React.Component {
+
+    static navigationOptions = {
+        title: "My Tasks",
+        tabBarIcon: ({tintColor})=>(
+            <Icon name='ios-paper' style={{color: tintColor}} />
+        )
+    };
+
     render() {
         return (
-            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                <Text>ToDoList!</Text>
-            </View>
+            <Container style={styles.container}>
+                <Content padder>
+                    <CardComponent imageSource="2"/>
+                    <CardComponent imageSource="2"/>
+                    <CardComponent imageSource="2"/>
+                    <CardComponent imageSource="2"/>
+                    <CardComponent imageSource="2"/>
+                    <CardComponent imageSource="2"/>
+                </Content>
+            </Container>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex:1,
+        backgroundColor: '#415180'
+    }
+});
+
+
