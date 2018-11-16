69852529264102475bb77e5c3300364c9a217af0
Aaron Brown <40678694+robot404@users.noreply.github.com>
2018-11-15 16:44:04 -0800

    Update WelcomeScreen.js

--- src/screens/WelcomeScreen.js
+++ src/screens/WelcomeScreen.js
@@ -1,21 +1,13 @@
 import {Component} from "react";
-import {Text, Button, Image, KeyboardAvoidingView, StyleSheet, View} from "react-native";
+import { Image, ImageBackground, StyleSheet, View} from "react-native";
+import Button from 'react-native-button';
 import React from "react";
-import tForm from 'tcomb-form-native';
 
-// Form and User initialize the user input fields.
-const Form = tForm.form.Form;
-const User = tForm.struct({
-    e_mail: tForm.String,
-    password: tForm.String
-});
 
 export default class WelcomeScreen extends Component {
     // Constructor initializes name, phoneNumber, houseID, and houseName to "".
     constructor(props) {
         super(props);
-        this.state = { e_mail: "", password: "" };
-        this.onChange=this.onChange.bind(this);
     }
 
     // Rids the sign up screen of the navigation bar that comes standard with 'react-navigation'.
@@ -24,126 +16,61 @@ export default class WelcomeScreen extends Component {
     };
 
     /**
-     * handleSubmit()
-     * When the "Sign Up" button is pressed, this function is called.
-     * It grabs the values in all input boxes and prints them to the console.
-     * Then proceeds to the next screen if no values were null.
-     * TODO: 'House Code' or 'House Name' can have a null field, but not both.
+     * handleSubmit_LogIn()
+     * When the "LOG IN" button is pressed, this function is called.
+     * It simply proceeds to the log in screen.
      */
     handleSubmit_LogIn = () => {
-        const value = this._form.getValue();
-        console.log('value: ', value);
-        // If password and email match database, log in.
-        if (value) {
-            this.props.navigation.navigate("TabNavigation");
-        }
+        this.props.navigation.navigate("LogIn");
     };
 
+    /**
+     * handleSubmit_SignUp()
+     * When the "Sign Up" button is pressed, this function is called.
+     * It simply proceeds to the sign up screen.
+     */
     handleSubmit_SignUp = () => {
         this.props.navigation.navigate("SignUp")
     };
 
     /**
-     * onChange()
-     * When a value is changed in one of the fields, this allows the information to remain present even after
-     * the screen refreshes.
-     * @param value
-     */
-    onChange(value) {
-        this.setState({value});
-    };
-
-    /**
      * render()
      * Layout for the sign up screen.
-     * TODO: The 'KeyboardAvoidingView' can potentially block the sign up button.
      * WARNING! Image path may need to be updated depending on directory hierarchy.
      * @returns {Layout}
      */
     render() {
+        const resizeMode = 'cover';
         return (
-            <KeyboardAvoidingView style={styles.container} behavior="padding" enabled>
-                <View style = {styles.container}>
-                    <View style={[styles.box_Logo]}>
-                        <Image style={{flex:1, height:undefined, width:undefined}}
-                               source={require("../assets/HouseMatesPNGLogo_noBackground.png")}
-                               resizeMode="contain"/>
-                        <Text style={[styles.text_Welcome]}>Doing the dirty work.</Text>
-                    </View>
-                    <View style={[styles.box_Form]}>
-                        <Form ref={c => this._form = c}
-                              type={User}
-                              value={this.state.value}
-                              onChange={this.onChange}
-                              options={options}/>
-                        <Button title="Log In" onPress={this.handleSubmit_LogIn} color='#425281'/>
-                    </View>
-                    <Button title="No account? Sign up!"
-                            onPress={this.handleSubmit_SignUp}
-                            color='#425281'/>
-
+            <ImageBackground style={{flex: 1, resizeMode}}
+                             source={require("../assets/HouseMates_splashBackground_crop02.png")}>
+                <View style={[styles.box_Logo]}>
+                    <Image style={{flex:1, height:undefined, width:undefined}}
+                           source={require("../assets/HouseMatesPNGLogo_dirtySlogan_ovalBackground.png")}
+                           resizeMode="contain"/>
                 </View>
-            </KeyboardAvoidingView>
+                <View style={{flex: 4}}>
+                    <View style={[styles.box_Form01]}>
+                        <Button style={{fontSize: 18, color: 'white', alignSelf: 'center', justifyContent: 'center'}}
+                                onPress={this.handleSubmit_LogIn}
+                                containerStyle={{ padding: 16, height: 60, width: 260, overflow: 'hidden', borderRadius: 2,
+                                    backgroundColor: '#283350' }}>
+                            LOG IN
+                        </Button>
+                    </View>
+                    <View style={[styles.box_Form02]}>
+                        <Button style={{fontSize: 18, color: 'white', alignSelf: 'center', justifyContent: 'center'}}
+                                onPress={this.handleSubmit_SignUp}
+                                containerStyle={{ padding: 16, height: 60, width: 260, overflow: 'hidden', borderRadius: 2,
+                                    backgroundColor: '#283350' }}>
+                            SIGN UP
+                        </Button>
+                    </View>
+                </View>
+            </ImageBackground>
         );
     }
 }
-
-// The following edits the style of the form. This format is required for the API.
-const formStyles = {
-    ...Form.stylesheet,
-    //
-    controlLabel: {
-        normal: {
-            color: 'white',
-            fontSize: 14
-        },
-        error: {
-            color: 'red',
-            fontSize: 14
-        }
-    },
-    textbox: {
-        normal: {
-            color: 'white',
-            borderWidth: 1,
-            borderColor:'#415180',
-            borderRadius: 4,
-            height: 36,
-            marginBottom: 5
-        },
-        error: {
-            color: 'white',
-            borderWidth: 1,
-            borderColor:'#415180',
-            height: 36,
-            marginBottom: 5
-        }
-    },
-    buttonText: {
-        fontSize: 18,
-        color: 'white'
-    },
-    button: {
-        backgroundColor: '#ffd344',
-        borderWidth: 1,
-        borderRadius: 8,
-        alignSelf: 'stretch',
-        justifyContent: 'center'
-    }
-};
-
-// The following edits the fields of the form. This format is required for the API.
-const options = {
-    fields: {
-        e_mail: {
-            label: 'Email:'
-        },
-        password: {
-            label: 'Password:'
-        },
-    },
-    stylesheet: formStyles,
-};
 
 // StyleSheet for the sign up screen.
 const styles = StyleSheet.create({
@@ -156,15 +83,23 @@ const styles = StyleSheet.create({
         flex: 4,
         marginTop: 60,
         justifyContent: 'flex-end',
-        backgroundColor: '#283350'
+        backgroundColor: 'transparent'
 
     },
-    box_Form: {
-        flex: 4,
-        backgroundColor: '#283350',
-        padding: 20,
+    box_Form01: {
+        flex: 6,
+        backgroundColor: 'transparent',
+        alignItems: 'center',
         justifyContent: 'flex-end',
+        padding: 10
 
+    },
+    box_Form02: {
+        flex: 4,
+        backgroundColor: 'transparent',
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+        padding: 10
     },
     text_Welcome: {
         color: '#ffd344',
