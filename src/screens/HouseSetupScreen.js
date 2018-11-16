8ad00c053406b646ab26d518a4fcd5a09cfeb7e9
Aaron Brown <40678694+robot404@users.noreply.github.com>
2018-11-15 16:40:36 -0800

    Updated UI for HouseSetupScreen.js

--- src/screens/HouseSetupScreen.js
+++ src/screens/HouseSetupScreen.js
@@ -1,15 +1,15 @@
 import {Component} from "react";
-import {Image, Text, Button, StyleSheet, View, TextInput} from "react-native";
+import {Image, Text, StyleSheet, View} from "react-native";
 import React from "react";
 import tForm from 'tcomb-form-native';
-
+import Button from 'react-native-button';
 const Form = tForm.form.Form;
 const User = tForm.struct({
     houseID: tForm.String,
 });
 
 export default class HouseSetupScreen extends Component {
-    // Constructor initializes name, phoneNumber, houseID, and houseName to "".
+    // Constructor initializes houseID to "".
     constructor(props) {
         super(props);
         this.state = {nameID: ""};
@@ -22,13 +22,12 @@ export default class HouseSetupScreen extends Component {
     };
 
     /**
-     * handleSubmit()
-     * When the "Sign Up" button is pressed, this function is called.
-     * It grabs the values in all input boxes and prints them to the console.
+     * handleSubmit_JoinHome()
+     * When the join button is pressed, this function is called.
+     * It grabs the values in the input box and prints them to the console.
      * Then proceeds to the next screen if no values were null.
-     * TODO: 'House Code' or 'House Name' can have a null field, but not both.
+     * TODO: add memeber to the household with the given houseID
      */
-
     handleSubmit_JoinHome = () => {
         const value = this._form.getValue();
         console.log('value: ', value);
@@ -37,6 +36,12 @@ export default class HouseSetupScreen extends Component {
         }
     };
 
+    /**
+     * handleSubmit_CreateHome()
+     * When the create button is pressed, this function is called.
+     * It simply proceeds to the next screen.
+     * TODO: add houseID generation here
+     */
     handleSubmit_CreateHome = () => {
             this.props.navigation.navigate("TabNavigation");
     };
@@ -48,47 +53,38 @@ export default class HouseSetupScreen extends Component {
     /**
      * render()
      * Layout for the sign up screen.
-     * TODO: The 'KeyboardAvoidingView' can potentially block the sign up button.
      * WARNING! Image path may need to be updated depending on directory hierarchy.
      * @returns {Layout}
      */
     render() {
         return (
             <View style={styles.container}>
-                <View style={styles.box_Spacer}>
-                    <Text style={styles.text_SubTitle}></Text>
+                <View style={styles.box_Option1}>
+                    <Form ref={c => this._form = c}
+                          type={User}
+                          value={this.state.value}
+                          onChange={this.onChange}
+                          options={options}/>
+                    <Button style={{fontSize: 14, color: 'white', justifyContent: 'center', alignSelf: 'center'}}
+                            onPress={this.handleSubmit_JoinHome}
+                            containerStyle={{ padding: 11, height: 45, overflow: 'hidden', borderRadius: 4,
+                                backgroundColor: '#6171A0' }}>
+                        JOIN EXISTING HOUSEHOLD
+                    </Button>
                 </View>
-                <View style={styles.box_Option}>
-                    <View style={styles.box_Fitter} >
-                        <Image style={{flex:1, height:undefined, width:undefined}}
-                               source={require("../assets/HouseMates_joinHouse_noBackground.png")}
-                               resizeMode="contain"/>
-                        <Text style={styles.text_SubTitle}>
-                            If you have received a house code from someone, enter it below.
-                        </Text>
-                        <Form ref={c => this._form = c}
-                              type={User}
-                              value={this.state.value}
-                              onChange={this.onChange}
-                              options={options}/>
-                        <Button title='Join Existing Home' color='#415180' onPress={this.handleSubmit_JoinHome}/>
-                    </View>
-                </View>
-                <View style={styles.box_Spacer}>
-                    <Text style={styles.text_SubTitle}></Text>
-                </View>
-                <View style={styles.box_Option}>
-                    <View style={styles.box_Fitter}>
-                        <Image style={{flex:1, height:undefined, width:undefined}}
-                               source={require("../assets/HouseMates_newHouse_noBackground.png")}
-                               resizeMode="contain"/>
+                <View style={styles.box_Option2}>
+                    <View style={styles.box_Text}>
                         <Text style={[styles.text_SubTitle2]}>
-                            Or start off on your own by creating a new home!
+                            Or...
                         </Text>
-                        <Button title='Create New Home' color='#415180' onPress={this.handleSubmit_CreateHome}/>
                     </View>
+                    <Button style={{fontSize: 14, color: 'white', justifyContent: 'center', alignSelf: 'center'}}
+                            onPress={this.handleSubmit_CreateHome}
+                            containerStyle={{ padding: 11, height: 45, overflow: 'hidden', borderRadius: 4,
+                                backgroundColor: '#6171A0' }}>
+                        CREATE NEW HOUSEHOLD
+                    </Button>
                 </View>
-
             </View>
         );
     }
@@ -109,39 +105,29 @@ const formStyles = {
     },
     textbox: {
         normal: {
-            color: '#415180',
+            color: 'white',
             borderWidth: 1,
-            borderColor:'#415180',
+            borderColor:'white',
             borderRadius: 4,
             height: 36,
             marginBottom: 5
         },
         error: {
-            color: '#415180',
+            color: 'red',
             borderWidth: 1,
-            borderColor:'#415180',
+            borderColor:'red',
             height: 36,
             marginBottom: 5
         }
     },
-    buttonText: {
-        fontSize: 18,
-        color: 'white'
-    },
-    button: {
-        backgroundColor: '#ffd344',
-        borderWidth: 1,
-        borderRadius: 8,
-        alignSelf: 'stretch',
-        justifyContent: 'center'
-    }
 };
 
 // The following edits the fields of the form. This format is required for the API.
 const options = {
     fields: {
         houseID: {
-            label: 'Enter House Code:'
+            label: ' ',
+            placeholder: 'Code',
         }
     },
     stylesheet: formStyles,
@@ -154,23 +140,26 @@ const styles = StyleSheet.create({
         flexDirection: 'column',
         justifyContent: 'space-around',
         backgroundColor: '#283350',
-        padding: 22,
+        paddingLeft: 16,
+        paddingRight: 16,
     },
-    box_Option: {
-        flex: 10,
-        backgroundColor: 'white',
+    box_Option1: {
+        flex: 12,
+        backgroundColor: '#415180',
         flexDirection: 'column',
-        justifyContent: 'flex-end'
+        justifyContent: 'flex-end',
+        padding: 10
     },
-    box_Spacer: {
-        flex: 0.5,
-        backgroundColor: 'transparent'
+    box_Option2: {
+        flex: 6,
+        backgroundColor: '#415180',
+        flexDirection: 'column',
+        justifyContent: 'flex-start',
+
+        padding: 10
     },
-    box_Fitter: {
-        flex: 1,
-        backgroundColor: 'transparent',
-        padding: 10,
-        justifyContent: 'flex-end'
+    box_Text: {
+        alignItems: 'center'
     },
     text_Title: {
         fontWeight: 'bold',
@@ -184,9 +173,56 @@ const styles = StyleSheet.create({
         padding: 0
     },
     text_SubTitle2: {
-        color: '#415180',
+        color: 'white',
         fontSize: 16,
         alignItems: 'center',
         marginBottom: 22
     },
 });
+
+/*
+<View style={styles.box_Spacer}>
+                    <Text></Text>
+                </View>
+                <View style={styles.box_Option}>
+                    <View style={styles.box_Fitter} >
+                        <Image style={{flex:1, height:undefined, width:undefined}}
+                               source={require("../assets/HouseMates_joinHouse_outlinedTEST_noBackground.png")}
+                               resizeMode="contain"/>
+                        <Text style={styles.text_SubTitle}>
+                            If you have received a house code from someone, enter it below.
+                        </Text>
+                        <Form ref={c => this._form = c}
+                              type={User}
+                              value={this.state.value}
+                              onChange={this.onChange}
+                              options={options}/>
+                        <Button style={{fontSize: 14, color: 'white', justifyContent: 'center', alignSelf: 'center'}}
+                                onPress={this.handleSubmit_JoinHome}
+                                containerStyle={{ padding: 11, height: 45, overflow: 'hidden', borderRadius: 4,
+                                    backgroundColor: '#415180' }}>
+                            JOIN EXISTING HOME
+                        </Button>
+                    </View>
+                </View>
+                <View style={styles.box_Spacer}>
+                    <Text></Text>
+                </View>
+                <View style={styles.box_Option}>
+                    <View style={styles.box_Fitter}>
+                        <Image style={{flex:1, height:undefined, width:undefined}}
+                               source={require("../assets/HouseMates_newHouse_outlinedTEST_noBackground.png")}
+                               resizeMode="contain"/>
+                        <Text style={[styles.text_SubTitle2]}>
+                            Or start off on your own by creating a new home!
+                        </Text>
+                        <Button style={{fontSize: 14, color: 'white', justifyContent: 'center', alignSelf: 'center'}}
+                                onPress={this.handleSubmit_CreateHome}
+                                containerStyle={{ padding: 11, height: 45, overflow: 'hidden', borderRadius: 4,
+                                    backgroundColor: '#415180' }}>
+                            CREATE NEW HOME
+                        </Button>
+                    </View>
+                </View>
+
+ */
