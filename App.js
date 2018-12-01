0c74d7db02c74a869e2c5886054f8a7e92dc6f9e
Amr Elfiky <elfikyamr1@gmail.com>
2018-11-30 16:26:27 -0800

    tasks fill cards, cards have usernames

--- App.js
+++ App.js
@@ -1,7 +1,5 @@
 global.Symbol = require('core-js/es6/symbol');
 require('core-js/fn/symbol/iterator');
-
-// collection fn polyfills
 require('core-js/fn/map');
 require('core-js/fn/set');
 require('core-js/fn/array/find');
